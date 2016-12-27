2997780237de33ce8587efd3d659993f7bac0338
Eric Loos <ericloos00@gmail.com>
2016-12-27 15:43:33 -0600

    login and logout devise is working

--- app/assets/javascripts/home/HomeController.js
+++ app/assets/javascripts/home/HomeController.js
@@ -6,35 +6,47 @@
       .controller('HomeController', HomeController)
 
 
-      function HomeController(Auth){
-        var vm = this 
+      function HomeController(Auth, $scope){
+        var vm = this
+        vm.logout = Auth.logout
+        vm.login = login 
 
         var credentials = {
         email: "ericloos00@gmail.com",
         password: "indiana10"
-      };
+      }
 
-      var config = {
-        headers: {
-          'X-HTTP-Method-Override':
-          'POST'
+        var config = {
+          headers: {
+            'X-HTTP-Method-Override': 'POST'
+          }
         }
-      };
-
+        function login(){
         Auth.login(credentials, config)
           .then(function(user){
-            console.log(user)
+            vm.user = user
           }, function(error){
             console.log(error)
           })
+        }
 
         Auth.currentUser()
           .then(function(user){
 
-          console.log(user);
+          vm.user = user
         }, function(error){
           console.log(error)
-        });
+        })
+
+        $scope.$on('devise:logout', function(event,user){
+          vm.user = {}
+        })
+
+        
+
+
+
+
       }
 
   }())
