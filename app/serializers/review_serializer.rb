a3bd5299c2890eba3702172a85bdfa297ec0cdca
Eric Loos <ericloos00@gmail.com>
2017-01-04 12:51:00 -0600

    adjusted serializers to show more information on home page of all reviews. Added active links

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,3 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id,  :awards, :box_office, :director, :language, :picture_url, :production, :rated, :year, :runtime, :title, :review_type, :imdb_id, :viewing_platform, :those_movie_guys_rating, :those_movie_guys_review, :writer, :imdb_rating, :imdb_votes, :tomato_consensus, :tomato_user,  :tomato_critics, :tomato_url
+  attributes :id,  :awards, :actors , :genres, :box_office, :director, :language, :picture_url, :production, :rated, :year, :runtime, :title, :review_type, :imdb_id, :viewing_platform, :those_movie_guys_rating, :those_movie_guys_review, :writer, :imdb_rating, :imdb_votes, :tomato_consensus, :tomato_user,  :tomato_critics, :tomato_url
 end
