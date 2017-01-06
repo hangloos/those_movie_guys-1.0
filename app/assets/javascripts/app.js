10762a83f578c1b57ac6a6cb6ec4f4c037553065
Eric Loos <ericloos00@gmail.com>
2017-01-06 13:21:21 -0600

    fixed login issues with devise. Altered links on header. Deleted admin in users. Will create an admin devise model to use for the admin login.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,9 +3,10 @@
 
     angular
       .module('those-movie-guys', ['templates', 'Devise', 'ui.router'])
-      // .config(function($httpProvider)
-      //   // $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = 
-      //   // $('meta[name=csrf-token]').attr('content');
-      // )
+       .config(function($httpProvider)  {
+        // for CSRF errors
+       $httpProvider.defaults.headers.common['X-CSRF-TOKEN'] = 
+       $('meta[name=csrf-token]').attr('content');
+       })
 
   }())
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
 
-  before_action :authenticate_user!, only: [:edit]
+  #before_action :authenticate_user!, only: [:edit]
 
   def show
     user = User.find_by_id(params[:id])
     render json: user
   end
 
-
-  def edit 
-
-  end
 
 
 end
